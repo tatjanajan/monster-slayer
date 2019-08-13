b841d33cf318c4acdb7e3080b1665c25edef5023
Tatjana Janković <tatjana.jankovic@ngit.hr>
2019-08-13 08:20:14 +0200

    Dodana metoda za pokretanje igre

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning:false
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        }
     }
     
 });
