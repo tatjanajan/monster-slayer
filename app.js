31050aac4bf8d454ab9d2daf5b4538ca256ec02a
Tatjana Janković <tatjana.jankovic@ngit.hr>
2019-08-13 10:28:00 +0200

    Refaktoriranje koda za attack uvođenjem metoda calculateDamage i checkWin

--- app.js
+++ app.js
@@ -12,26 +12,15 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function() {
-            var max = 10;
-            var min = 3;
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.monsterHealth -= damage;
-
-            if(this.monsterHealth <= 0) {
-                alert('You won!');
-                this.gameIsRunning = false;
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if(this.checkWin()) {
                 return;
             }
+            
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
 
-            max = 12;
-            min = 5;
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.playerHealth -= damage;
-
-            if(this.playerHealth <= 0) {
-                alert('You lost!');
-                this.gameIsRunning = false;
-            }
+           
         },
         specialAttack: function() {
 
@@ -41,6 +30,28 @@ new Vue({
         },
         giveUp: function() {
 
+        },
+        calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            if(this.monsterHealth <= 0) {
+                if(confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false; 
+                }
+                return true;
+            }
+            else if(this.playerHealth <= 0) {
+                if(confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false; 
+                }
+                return true;
+            }
+            return false;
         }
     }
     
